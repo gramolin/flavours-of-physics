03f806b5fcc156a941d68b38ebfbeda711a91b75
gramolin <gramolin@gmail.com>
2015-12-11 03:17:17 +0600

    Minor changes

--- parameters.py
+++ parameters.py
@@ -3,25 +3,17 @@
 # Random seed:
 random_state = 1
 
-# Weight for the first booster:
+# Weight for the first classifier:
 w1 = 0.78
 
 # Numbers of trees:
-num_trees1 = 200 # Booster 1
-num_trees2 = 100 # Booster 2
+num_trees1 = 200 # Classifier 1
+num_trees2 = 100 # Classifier 2
 
-# Parameters of the first booster:
-params1 = {'objective': 'binary:logistic',
-           'eta': 0.05,
-           'max_depth': 4,
-           'scale_pos_weight': 5.,
-           'silent': 1,
-           'seed': random_state}
-
-# Parameters of the second booster:
-params2 = {'objective': 'binary:logistic',
-           'eta': 0.05,
-           'max_depth': 4,
-           'scale_pos_weight': 5.,
-           'silent': 1,
-           "seed": random_state}
+# Parameters of the classifiers:
+params = {'objective': 'binary:logistic',
+          'eta': 0.05,
+          'max_depth': 4,
+          'scale_pos_weight': 5.,
+          'silent': 1,
+          'seed': random_state}
--- train.py
+++ train.py
@@ -1,23 +1,22 @@
 # https://github.com/gramolin/flavours-of-physics
 
-import csv
-import pandas as pd
-import xgboost as xgb
+import pandas, xgboost, features, parameters
 
-import features
-import parameters
-
-# Read the training set:
-train = pd.read_csv('data/training.csv', index_col='id')
+# Read the training dataset:
+train = pandas.read_csv('data/training.csv', index_col='id')
 train = train[train['min_ANNmuon'] > 0.4]
 
 # Add extra features:
 train = features.add_features(train)
 
-# Train the first XGBoost booster:
-bst1 = xgb.train(parameters.params1, xgb.DMatrix(train[features.list1], train['signal']), parameters.num_trees1)
+# Train the first (geometric) XGBoost classifier:
+bst1 = xgboost.train(parameters.params,
+                     xgboost.DMatrix(train[features.list1],
+                     train['signal']), parameters.num_trees1)
 bst1.save_model('bst1.model')
 
-# Train the second XGBoost booster:
-bst2 = xgb.train(parameters.params2, xgb.DMatrix(train[features.list2], train['signal']), parameters.num_trees2)
+# Train the second (kinematic) XGBoost classifier:
+bst2 = xgboost.train(parameters.params,
+                     xgboost.DMatrix(train[features.list2],
+                     train['signal']), parameters.num_trees2)
 bst2.save_model('bst2.model')
